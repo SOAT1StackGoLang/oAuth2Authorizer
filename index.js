f7f51f2b815d49dfc7384be422c7a863cf49772a
Murillo <mm@in3d.com.br>
2023-11-03 12:09:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,29 @@ const { CognitoJwtVerifier } = require("aws-jwt-verify");
 // so the cache can be reused for subsequent invocations. Then, only during the
 // first invocation, will the verifier actually need to fetch the JWKS.
 const verifier = CognitoJwtVerifier.create({
-  userPoolId: "us-east-1_ZKZPzWYFr",
+  userPoolId: "us-east-1_rMynssSb6",
   tokenUse: "access",
-  clientId: "7kgaqt1ne3k01q149v4bdehuf7",
+  clientId: "17epfms5np5l92dsp4jk68m34d",
 });
 exports.handler = async (event) => {
+  console.log('event.headers.Authorization', event.headers.Authorization);
+  
   if (!event.headers.Authorization) {
+    console.log('Returning not authorized');
     return {
       isAuthorized: false,
     };
   }
-  try {
+  try {    
+    console.log('Verifying in cognito');
     await verifier.verify(event.headers.Authorization);
+    
+    console.log('Returning authorized');
     return {
       isAuthorized: true,
     };
   } catch (e) {
+    console.log('Returning not authorized');
     console.error(e);
     return {
       isAuthorized: false,
